320893d6ff2a53a664043122d0f600a7ee8d4b73
0xBlue <53667079+polaris430@users.noreply.github.com>
2022-10-19 12:33:43 -0400

    Add files via upload
    intial commit

---
+++ main.js
@@ -0,0 +1,18 @@
+let counter = 0;
+let counterReset = counter - counter;
+let countEl = document.getElementById("count-el");
+let paraEl = document.getElementById("previous");
+
+console.log(countEl);
+
+function increment() {
+  counter += 1;
+  countEl.textContent = counter;
+}
+
+function save() {
+  //get counter from increment function and display on console
+  let countStr = " " + counter + " - ";
+  paraEl.textContent = paraEl.textContent + countStr;
+  countEl.textContent = counterReset;
+}
